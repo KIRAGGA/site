b2531dc7c249f7574131f8415340f41bea3b4def
KIRAGGA <preagkan@gmail.com>
2020-02-21 22:22:47 +0300

    Working on the blades

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Home</title>
+        <title>about us</title>
     </head>
     <body>
-        <p> This is the home page</p>
+        <p> This is the contact about us page</p>
         <ul>
             <li><a href="/about">about</a></li>
             <li><a href="/contact">Contact</a></li>
