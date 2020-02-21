60cb5a0d1b7d93a0c11b75d1b8c0f4aae815a623
KIRAGGA <preagkan@gmail.com>
2020-02-21 22:33:29 +0300

    working on the blades

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        @yield('content')
+        <ul>
+            <li><a href="/about">about</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Home</title>
-    </head>
-    <body>
-        <p> This is the home page</p>
-        <ul>
-            <li><a href="/about">about</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </body>
-</html>
+@extends('layout')
+
+
+@section('content')
+<p> This it my website</p>
+
+@endsection
