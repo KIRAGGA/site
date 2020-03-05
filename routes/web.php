6d44738ea68b3f58a899f58f802cd8b1af3e4941
KIRAGGA <preagkan@gmail.com>
2020-03-05 19:46:32 +0300

    working on the form

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
 Route::get('/projects', 'ProjectsController@index');
 Route::post('/projects', 'ProjectsController@store');
 Route::get('/projects/create', 'ProjectsController@create');
+Route::get('/projects/{project}', 'ProjectsController@show');
+Route::post('/projects', 'ProjectsController@store');
+Route::patch('/projects/{project}', 'ProjectsController@update');
+Route::delete('/projects/{project}', 'ProjectsController@destory');
