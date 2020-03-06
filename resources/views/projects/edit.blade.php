42bd51dad2853beccb9c0e7290294513ae5e57eb
KIRAGGA <preagkan@gmail.com>
2020-03-06 08:41:13 +0300

    working on patch and delete request

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -6,6 +6,10 @@
     <form action="">
         <div class="field">
             <label for="title" class="label">Title</label>
+
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
         </div>
     </form>
 @endsection
