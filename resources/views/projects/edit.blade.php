ece4473141eac039bbbceea75a434a27289ad578
KIRAGGA <preagkan@gmail.com>
2020-03-06 09:04:51 +0300

    working on patch and delete request

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <h1>Edit project</h1>
-    <form action="/projects/{{$project->id}}" >
+    <form action="/projects/{{$project->id}}" method="POST" >
         <div class="field">
             <label for="title" class="label">Title</label>
 
