33b4eb080fa93e99c45a245641e9fe8d8683fc96
KIRAGGA <preagkan@gmail.com>
2020-03-06 08:40:01 +0300

    working on patch and delete request

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,4 +3,9 @@
 
 @section('content')
     <h1>Edit project</h1>
+    <form action="">
+        <div class="field">
+            <label for="title" class="label">Title</label>
+        </div>
+    </form>
 @endsection
