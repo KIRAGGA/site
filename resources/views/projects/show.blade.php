b2cdc97bdf2bfd5240569a36f193070a2319ef1a
KIRAGGA <preagkan@gmail.com>
2020-03-12 21:19:25 +0300

    Working on eloquent relationship

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,6 +4,12 @@
 <h1>{{$project->title}}</h1>
 <p>{{$project->description}}</p>
 
+<div>
+    @foreach ($project->tasks as $tasks)
+        
+    @endforeach
+</div>
+
 <p>
     <a href="/projects/{{$project->id}}/edit">edit</a>
 </p>
