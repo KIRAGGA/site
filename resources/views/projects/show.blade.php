8561a9f0b26024fe17dfb310dad8efd6a5f9e3db
KIRAGGA <preagkan@gmail.com>
2020-03-14 19:32:28 +0300

    Working on form action considerations

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,6 +16,7 @@
             <div>
             <form method="POST" action="/tasks/{{ $task->id}}">
                     @method('PATCH')
+                    @csrf
                     <label for="completed" class="checkbox">
                         <input type="checkbox" name="completed" onchange="this.form.submit()">
                         {{$task->description}}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
 Route::resource('projects', 'ProjectsController');
+Route::patch('/tasks/{task}', 'projectTasksController@update');
 
 // Route::get('/projects', 'ProjectsController@index');
 // Route::post('/projects', 'ProjectsController@store');
