136e41131417784f95b2e29a65b75983d0bf11d8
KIRAGGA <preagkan@gmail.com>
2020-03-22 22:12:47 +0300

    Working on serice providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+        $this->app->blind(
+        \App\Repositories\UserRepository::class,
+        \App\Repositories\DbUserRepository::class,
+    );
     }
 
     /**
--- app/Providers/SocialServiceProvider.php
+++ app/Providers/SocialServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use App\Providers\Twitter;
 use Illuminate\Support\ServiceProvider;
 
 class SocialServiceProvider extends ServiceProvider
