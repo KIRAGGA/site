6fa5a9d2731a771d821ec4327eeee4ad59e48dc6
KIRAGGA <preagkan@gmail.com>
2020-03-22 23:26:05 +0300

    working on cpnfiguration and environments

--- config/services.php
+++ config/services.php
@@ -30,4 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+
+    'twitter' => [
+        'key' => env('TWITTER_KEY'),
+        'secret' => env('TWITTER_SECRET')
+    ]
+
+
 ];
